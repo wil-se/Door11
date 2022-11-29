f0abc92df8e169dffaa06850239bc16fc857fb60
wil-se <sebastiani.1753672@studenti.uniroma1.it>
2022-11-29 19:38:32 +0700

    fix

--- frontend-admin/src/_components/ImageGalleryPreview.jsx
+++ frontend-admin/src/_components/ImageGalleryPreview.jsx
@@ -26,7 +26,7 @@ export function ImageGalleryPreview(props) {
   ++number
   return (
     <>
-      <Card style={{ height: 330, width: 200 }} className="mb-3 mx-2">
+      <Card style={{ height: 420, width: 200 }} className="mb-3 mx-2">
         <Card.Header className='text-center'>
           <div className='text-center'>
             <b>{number}</b>
